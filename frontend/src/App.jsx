cd47e6238130e638d7a1e05d053b90e316d2625b
Nikkiraj4 <nikitakumari4614@gmail.com>
2026-07-07 01:08:56 +0530

    Initial React project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>Zerodha Clone</h1>
+
+      <p>Welcome to our MERN Project 🚀</p>
+    </>
+  );
+}
+
+export default App;
