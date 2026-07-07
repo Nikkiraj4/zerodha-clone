618ae333d916f80e097d2b5ccbca3dff1bbd41d2
Nikkiraj4 <nikitakumari4614@gmail.com>
2026-07-07 17:34:07 +0530

    Build responsive navigation bar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav>
       <h2>Zerodha</h2>
+
+      <div>
+        <Link to="/signup">Signup</Link>
+        {" | "}
+        <Link to="/about">About</Link>
+        {" | "}
+        <Link to="/products">Products</Link>
+        {" | "}
+        <Link to="/pricing">Pricing</Link>
+        {" | "}
+        <Link to="/support">Support</Link>
+      </div>
     </nav>
   );
 }
